0f795d84eebe9ec82d2fddecd09bc464ddcfd258
huangyuuu1999 <huangyuuu1999@gmail.com>
2024-06-19 13:21:36 +0800

    delete

--- python_/example/for_i_in.py
+++
@@ -1,8 +0,0 @@
-nums = [2, 4, 5, 7, 9]
-for i in nums:
-    i = 7
-print(nums)
-
-for j in range(len(nums)):
-    nums[j] = 7
-print(nums)
--- python_/example/i_scope.py
+++
@@ -1,4 +0,0 @@
-i = 2
-for i in range(8):
-    print('x', end=' ')
-print(i) # i = ?
