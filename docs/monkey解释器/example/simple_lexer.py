b8fefeea12c9f762dd0adc18c1a63de404d0f796
huangyuuu1999 <huangyuuu1999@gmail.com>
2024-06-28 15:03:56 +0800

    sign

--- docs/monkey解释器/example/simple_lexer.py
+++ docs/monkey解释器/example/simple_lexer.py
@@ -4,7 +4,6 @@ x =x -3;
 
 class Lexer:
     def __init__(self, source_code):
-        self.token_list = []
         self.source = source_code
         self.cur_pos = -1
         self.next_pos = 0
@@ -12,7 +11,7 @@ class Lexer:
 
     def read_char(self):
         if self.next_pos >= len(self.source):
-            self.cur_ch = ''
+            self.cur_ch = 'EOF'
         else:
             self.cur_ch = self.source[self.next_pos]
         self.cur_pos = self.next_pos
@@ -29,3 +28,29 @@ for _ in range(len(source)):
         print(r'\n', lx.cur_pos, lx.next_pos, sep='\t')
     else:    
         print(c, lx.cur_pos, lx.next_pos, sep='\t')
+
+# 简单的实现 不用oop
+s = "let x = 5; x = x + 7;"
+cur_pos, next_pos = -1, 0
+cur_ch = ''
+read_ch = []
+while next_pos < len(s):
+    cur_ch = s[next_pos]
+    read_ch.append(cur_ch)
+    cur_pos = next_pos
+    next_pos += 1
+print(read_ch)
+
+
+# 简单的实现 不用oop
+s = "let x = 5; x = x + 7;"
+cur_pos, next_pos = -1, 0
+cur_ch = ''
+read_ch = []
+while next_pos < len(s):
+    cur_ch = s[next_pos]
+    if cur_ch in ['=', '+', '(', ')', '{', '}']:  # 可以拓展这个列表，完全取决于如何约定
+        read_ch.append(cur_ch)
+    cur_pos = next_pos
+    next_pos += 1
+print(read_ch)
